0c4b5a1a44b160075554ba8124bbeeb8a70e8fb2
brunouno <bruno.navarreteon@gmail.com>
2017-08-15 12:01:33 -0500

    checkForQuote

--- public/js/main.js
+++ public/js/main.js
@@ -27,21 +27,46 @@
 			};
 		}
 
+		$scope.checkForQuote = function(id,quote,category){
+			var userLists = $scope.user.lists;
+			for (var i = 0; i < userLists.length; i++) {
+				if( userLists[i].category === category ){
+					var currentList = i;
+					for (var i2 = 0; i2 < userLists[currentList].quotes.length; i2++) {
+						if( category === 'trump' ){
+							if( userLists[currentList].quotes[i2].quote === quote ){
+								return $scope.saved = true;
+							} else {
+								$scope.saved = false;
+							}
+						} else {
+							if( userLists[currentList].quotes[i2]._quote_id === id ){
+								return $scope.saved = true;
+							} else {
+								$scope.saved = false;
+							}
+						}
+					}
+				}
+			};
+		}
+
 		$scope.getQuote = function(category){
-			$scope.currentCat = category;
-			var quoteData = [];
 			if( category === 'code' ){
 				quoteService.getQuote('http://quotes.stormconsultancy.co.uk/random.json',function(data){
 					$scope.setNewQuote(data.data.id, data.data.quote, data.data.author,category);
+					$scope.checkForQuote(data.data.id, data.data.quote, category);
 				});	
 			} else if( category === 'trump' ){
 				quoteService.getQuote('https://api.whatdoestrumpthink.com/api/v1/quotes/random',function(data){
 					$scope.setNewQuote(null, data.data.message, 'Donald J. Trump',category);
+					$scope.checkForQuote(null, data.data.message, category);
 				});
 			} else if( category === 'misc' ){
 				quoteService.getQuote('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',function(data){
 					var strippedQuote = data.data[0].content.replace(/<p>|<\/p>/gi,'');
 					$scope.setNewQuote(data.data[0].ID, strippedQuote, data.data[0].title,category);
+					$scope.checkForQuote(data.data[0].ID, strippedQuote, category);
 				});
 			}
 		}
@@ -53,6 +78,7 @@
 					console.log('hello from controller');
 				});
 				$scope.warning = '';
+				$scope.saved = true;
 				quoteService.getUser(function(user){
 					$scope.user = user.data;
 				});
